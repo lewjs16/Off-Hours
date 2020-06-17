1b579f3d445c4c6dae51ceeaf869d3e187a5771c
Raul Dutta <duttar@umich.edu>
2020-06-17 13:21:05 -0400

    set up basics of database

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# initializes app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# initializes database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init ma
+ma = Marshmallow(app)
+
+class Streams(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80),nullable=False)
+    subject = db.Column(db.String(80),nullable=False)
+    ownerid = db.Column(db.Integer,nullable=False)
+    streamimg = db.Column(db.String(80),nullable=False)
+    vidnum = db.Column(db.Integer,nullable=False)
+
+class Users(db.Model):
+    id = db.Column(db.Integer,db.ForeignKey('streams.ownerid'),primary_key=True)
+    username = db.Column(db.String(80),nullable=False)
+    userimg = db.Column(db.String(80),nullable=False)
+
+class Questions(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    streamid = db.Column(db.Integer,db.ForeignKey('streams.id'),nullable=False)
+    message = db.Column(db.Text,nullable=True)
+    image = db.Column(db.String(80),nullable=True)
+    accepted = db.Column(db.Boolean,default=False,nullable=False)
+    completed = db.Column(db.Boolean,default=False,nullable=False)
+
+
+
+@app.route('/',methods=['GET'])
+def get():
+    return jsonify({'msg':'Hellow World'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
