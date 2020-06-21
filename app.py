e3c19a18a5b0ca8fa399cbc6f60c77c2a17c65b7
lewjs16 <lewjs16@gmail.com>
2020-06-20 23:48:19 -0400

    test with secret

--- app.py
+++ app.py
@@ -193,9 +193,10 @@ def login():
       
       	# get tokenfrom Twitch API
         client_id = "hgzp49atoti7g7fzd9v4pkego3i7ae"
+        secret = "q1hod728vx9dpxnxoypyngsbvrn4kg"
         auth_code = "code"
         redirect_uri = "https://off-hours-backend.herokuapp.com/login"
-        data = requests.post("https://id.twitch.tv/oauth2/token?client_id="+client_id+"&code="+auth_code+"&grant_type=authorization_code&redirect_uri="+redirect_uri)
+        data = requests.post("https://id.twitch.tv/oauth2/token?client_id="+client_id+"&client_secret="+secret +"&code="+auth_code+"&grant_type=authorization_code&redirect_uri="+redirect_uri)
         
         return jsonify(data.text)
         # store token and other info
