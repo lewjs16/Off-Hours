1e3fa7376a72ea1957dd663b225aa119e5f8e3dd
lewjs16 <lewjs16@gmail.com>
2020-06-21 02:10:21 -0400

    redirect uri change

--- app.py
+++ app.py
@@ -194,9 +194,8 @@ def login():
       	# get tokenfrom Twitch API
         client_id = "hgzp49atoti7g7fzd9v4pkego3i7ae"
         auth_code = flask.request.args.get("code", default="",type=str)
-        redirect_uri = "https://localhost:3000/login/"
-        state = "fgb7pgr2g24vwrfl6auc1miz8jtsgg"
-        data = requests.post("https://id.twitch.tv/oauth2/token?client_id="+client_id+"&code="+auth_code+"&grant_type=authorization_code&redirect_uri="+redirect_uri+"&state=" + state)
+        redirect_uri = "https://offhours.herokuapp.com/login/"
+        data = requests.post("https://id.twitch.tv/oauth2/token?client_id="+client_id+"&code="+auth_code+"&grant_type=authorization_code&redirect_uri="+redirect_uri)
         
         # store token and other info
         flask.session['token'] = json.loads(data)['access_token']
