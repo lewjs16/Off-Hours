ae7e277c599d97f3829a984d9c46efe7432a3035
lewjs16 <lewjs16@gmail.com>
2020-06-22 16:01:03 -0400

    test

--- app.py
+++ app.py
@@ -189,12 +189,16 @@ def login_check(id):
 def login():
     # dont want to make a new user each time front end checks if we are logged in
     # only when we log in (POST) AND when the user is not already in our database
+    return jsonify(
+        username = "hello",
+        name = "test",
+        loggedin = "test"
+    )
     if app.request.method == 'POST':
-      
+        
       	# get tokenfrom Twitch API
         client_id = "hgzp49atoti7g7fzd9v4pkego3i7ae"
         auth_code = flask.request.args.get("code", default="",type=str)
-        auth_code.headers.add('Access-Control-Allow-Origin', 'https://offhours.herokuapp.com')
         redirect_uri = "https://offhours.herokuapp.com/login/"
         data = requests.post("https://id.twitch.tv/oauth2/token?client_id="+client_id+"&code="+auth_code+"&grant_type=authorization_code&redirect_uri="+redirect_uri)
         
