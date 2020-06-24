e02bb8b90464f4d32f2123a4073f0e70db5bfff4
lewjs16 <lewjs16@gmail.com>
2020-06-24 00:04:46 -0400

    change to CORS ACCESS

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 #cors = CORS(app, resources={r"/login/.*": {"origins": "*"}})
-cors = CORS(app, resources={r"/login": {"origins": "*"}})
+#cors = CORS(app, resources={r"/login": {"origins": "*"}})
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #For login 
