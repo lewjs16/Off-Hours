13167295d2c42c828bcdd073dd779449a3f63427
Raul Dutta <duttar@umich.edu>
2020-06-25 11:48:00 -0400

    turned to data

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
-import twitch
 
 # initializes app
 app = Flask(__name__)
