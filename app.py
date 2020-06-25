2c0cc5e7d25bbe1b09c53c0503db41325e22019b
lewjs16 <lewjs16@gmail.com>
2020-06-25 14:59:39 -0400

    secret key

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import flask
 import flask_cors
 import flask_sqlalchemy
 import flask_marshmallow
+import flask_session
 import datetime
+from flask_session import Session
 from flask import Flask, request, jsonify, session
 from flask import make_response, current_app
 from flask_cors import CORS, cross_origin
@@ -15,7 +17,13 @@ from datetime import datetime
 
 # initializes app
 app = Flask(__name__)
+SESSION_TYPE = "redis"
+PERMANENT_SESSSION_LIFETIME = 1800
+app.config.update(SECRET_KEY =  os.urandom(24))
+app.config.from_object(__name__)
+Session(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 #cors = CORS(app, resources={r"/login/.*": {"origins": "*"}})
 #cors = CORS(app, resources={r"/login": {"origins": "*"}})
 CORS(app)
@@ -35,153 +43,124 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 #Classes 
-class Subjects(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    subject = db.Column(db.String(80),nullable=False)
+# class Subjects(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     subject = db.Column(db.String(80),nullable=False)
 
-    def __init__(self,id,subject):
-        self.id = id
-        self.subject = subject
+#     def __init__(self,id,subject):
+#         self.id = id
+#         self.subject = subject
 
-class Streams(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(80),nullable=False)
-    subject = db.Column(db.String(80),db.ForeignKey('subjects.id'),nullable=False)
-    ownerid = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
-    vidnum = db.Column(db.Integer,nullable=True)
+# class Streams(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     title = db.Column(db.String(80),nullable=False)
+#     subject = db.Column(db.String(80),db.ForeignKey('subjects.id'),nullable=False)
+#     ownerid = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+#     vidnum = db.Column(db.Integer,nullable=True)
 
-    def __init__(self,title,subject,ownerid):
-        self.title=title
-        self.subject=subject
-        self.ownerid=ownerid
+#     def __init__(self,title,subject,ownerid):
+#         self.title=title
+#         self.subject=subject
+#         self.ownerid=ownerid
 
 class Users(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80),nullable=False)
     name = db.Column(db.String(80),nullable=False)
-    token = db.Column(db.Integer,nullable=True)
-    time = db.Column(db.Integer,nullable=True)
-    loggedin = db.Column(db.Boolean, nullable=False)
+    live = db.Column(db.Boolean, nullable=False)
 
-    def __init__(self,username, name):
+
+    def __init__(self,username, name,live):
         self.username = username
         self.name = name
+        self.live = live
 
 class Questions(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    streamid = db.Column(db.Integer,db.ForeignKey('streams.id'),nullable=False)
+    # streamid = db.Column(db.Integer,db.ForeignKey('streams.id'),nullable=False)
     userid = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
-    offervalue = db.Column(db.Integer,nullable=False)
+    # offervalue = db.Column(db.Integer,nullable=False)
     message = db.Column(db.Text,nullable=True)
-    image = db.Column(db.String(80),nullable=True)
-    accepted = db.Column(db.Boolean,default=False,nullable=False)
+    # image = db.Column(db.String(80),nullable=True)
+    # accepted = db.Column(db.Boolean,default=False,nullable=False)
     completed = db.Column(db.Boolean,default=False,nullable=False)
     time = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
 
-    def __init__(self,streamid,userid,message,image):
-        self.streamid = streamid
+    def __init__(self,userid,message):
         self.userid = userid
-        if (message):
-            self.message = message
-        if (image):
-            self.image = image
+        self.message = message
+        self.completed = False
 
 #Schema 
-class Subjects_Schema(ma.Schema):
-    class Meta:
-        fields = ('id','subject')
+# class Subjects_Schema(ma.Schema):
+#     class Meta:
+#         fields = ('id','subject')
 
-class Streams_Schema(ma.Schema):
-    class Meta:
-        fields = ('id','title', 'subject', 'ownerid')
+# class Streams_Schema(ma.Schema):
+#     class Meta:
+#         fields = ('id','title', 'subject', 'ownerid')
 
 class User_Schema(ma.Schema):
     class Meta: 
-        fields = ('id','username', 'name')
+        fields = ('id','username', 'name','live')
 
 class Questions_Schema(ma.Schema):
     class Meta: 
-        fields = ('id','streamid','userid', 'message', 'image')
+        fields = ('id','userid', 'message')
 
 
-#Init Schema
-streams_schema_reg = Streams_Schema()
+# Init Schema
+# streams_schema_reg = Streams_Schema()
 user_schema_reg = User_Schema()
 questions_schema_reg = Questions_Schema()
-subjects_schema_reg = Subjects_Schema()
+# subjects_schema_reg = Subjects_Schema()
 
-streams_schema_multi = Streams_Schema(many=True)
+# streams_schema_multi = Streams_Schema(many=True)
 user_schema_multi = User_Schema(many=True)
 questions_schema_multi = Questions_Schema(many=True)
-subjects_schema_multi = Subjects_Schema(many = True)
+# subjects_schema_multi = Subjects_Schema(many = True)
 
-streams_schema_single = Streams_Schema(many = False)
+# streams_schema_single = Streams_Schema(many = False)
 user_schema_single = User_Schema(many=False)
 questions_schema_single = Questions_Schema(many=False)
-subjects_schema_single = Subjects_Schema(many = False)
+# subjects_schema_single = Subjects_Schema(many = False)
 
 #START STREAM FUNCTIONS---------------------------------------------------------------------
+@app.route('/stream', methods = ['GET','POST'])
+def get_stream():
+    username = request.args.get("username", default=flask.session["username"],type=str)
+    user = Users.query.filter_by(username=username).first()
+    live = user.live
+    
+    if flask.request.method == 'POST':
+        tmp = request.args.get("live", default="",type=str)
+        if(tmp=='true'):
+            user.live=True
+            live = True
+        else:
+            user.live=False
+            live = False
+        db.session.commit()
+    return jsonify(
+        username = user.username,
+        name = user.name,
+        isLive = live
+    )
 
-#Adding a stream
-@app.route('/addStream', methods = ['POST'])
-def add_stream():
-    title = request.json['title']
-    subject = request.json['subject']
-    ownerid = request.json['ownerid']
+@app.route('/all', methods = ['GET'])
+def get_stream():
+    streamers = []
+    query = Users.query.filter_by(live=True).all()
+    for user in query:
+        streamers.append({
+            'username': user.username,
+            'name' : user.name
+        })
+    return jsonify(streamers=streamers)
 
-    new_stream = Streams(title, subject, ownerid)
-
-    db.session.add(new_stream)
-    db.session.commit()
-
-    return streams_schema_reg.jsonify(new_stream)
-
-# Getting all streams
-@app.route('/Streams', methods = ['GET'])
-def get_streams():
-    all_streams = Streams.query.all()
-    #serialize the queryset
-    result = streams_schema_multi.dump(all_streams)
-    return jsonify(result)
-
-# Gettings a single stream
-@app.route('/stream/<id>', methods = ['GET','POST'])
-def get_stream(id):
-     stream = Streams.query.get(id)
-
-
-     result = streams_schema_single.dump(stream)
-     return jsonify(result)
-
-# Update single stream
-@app.route('/StreamUpdate/<id>', methods = ['PUT'])
-def update_stream(id):
-    title = request.json['title']
-    subject = request.json['subject']
-    ownerid = request.json['ownerid']
-
-    update_stream = Streams.query.get(id)
-
-    update_stream.title = title
-    update_stream.subject = subject
-    update_stream.ownerid = ownerid
-
-    db.session.commit()
-
-    return streams_schema_single.jsonify(update_stream)
-
-# Delete stream
-@app.route('/StreamDelete/<id>', methods = ['DELETE'])
-def delete_stream(id):
-    stream = Streams.query.get(id)
-    db.session.delete(stream)
-    db.session.commit()
-
-    return streams_schema_reg.jsonify(stream)
-
-#END STREAM FUNCTIONS---------------------------------------------------------------------
 
 #START USER FUNCTIONS---------------------------------------------------------------------
+# Gettings a single stream
 
 
 # login/add user to database
@@ -243,8 +222,4 @@ def test():
     return jsonify({"test": "working"})
 #END TEST FUNCTIONS---------------------------------------------------------------------
 
-
-if __name__ == '__main__':
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True)
 
